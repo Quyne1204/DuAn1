d7688aa1b4d4f46d28b7691b4ce1180723b66891
Quyne1204 <nvq12042003@gmail.com>
2022-11-07 13:24:57 +0700

    up file

---
+++ javascript/active.js
@@ -0,0 +1,27 @@
+const tabActive = document.querySelectorAll('.tab');
+const tabBanner = document.querySelectorAll('.tab-banner');
+let actIndex = 0;
+
+var indexActive = (act) => {
+    tabActive.forEach(tab => {
+        tab.classList.remove('active');
+    })
+
+    tabBanner.forEach(banner => {
+        banner.classList.remove('active');
+    })
+
+
+    tabActive[act].classList.add('active');
+    tabBanner[act].classList.add('active');
+
+}
+
+
+tabActive.forEach((tab, i) => {
+    tab.addEventListener('click', () => {
+        indexActive(i);
+        actIndex = i;
+    })
+
+})

d8679ba04fe4289d6bd02066727325a03b2d4532
thanhNguyen2k3 <thanhs2fo4@gmail.com>
2022-11-11 13:29:23 +0700

    sửa lại lỗi load sản phẩm

--- pages/container.php
+++ pages/container.php
@@ -238,15 +238,25 @@
 
                     <!-- Item -->
                     <?php
-                        foreach ($list_products as $products) {
-                            extract($products);
-                            $price= product_price($money);
-                            echo '
-                                <a href="index.php?act=detail&id_products='.$id_product.'&id_categories='.$type_id.'" class="product-item">
+                    function product_price($priceFloat)
+                    {
+                        $symbol = 'đ';
+                        $symbol_thousand = '.';
+                        $decimal_place = 0;
+                        $price = number_format($priceFloat, $decimal_place, '', $symbol_thousand);
+                        return $price . $symbol;
+                    }
+                    foreach ($list_products as $products) {
+                        extract($products);
+                        $price = product_price($money);
+                        $priceFloat = $money;
+                        echo '
+                                <a href="index.php?act=detail&id_products=' . $id_product . '&id_categories=' . $type_id . '" class="product-item">
                                     <div class="inner-item">
-                                        <img class="laptop-img" src="./images/sanpham/'.$img.'" >
-                                        <p class="inner-name">'.$products_name.'</p>
-                                        <p class="inner-price">'.$price.'</p>
+                                        <img class="laptop-img" src="./images/sanpham/' . $img . '" >
+                                        <p class="inner-name">' . $products_name . '</p>
+                                        <p class="inner-price">' . $price . '</p>
+                                        <p class="inner-price">' . product_price($priceFloat) . '</p>
                                         <p class="sale"><del>17.990.000<span>đ</span></del> <span class="sold">-24.46%</span> </p>
                                     </div>
                                 </a>
